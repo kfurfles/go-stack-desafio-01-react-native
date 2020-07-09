1fc81fd79e30d5b3fcd5359270bf7c0fe4b09d3f
furflesx <kelvinsilva.ksv@gmail.com>
2020-07-09 14:24:05 -0300

    desafio

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,19 @@ const api = axios.create({
   baseURL: "http://localhost:3333",
 });
 
+export const getRepositories = async () => {
+  const {data} = await api.get('/repositories')
+  return data
+}
+
+export const newRepository = async (repo) => {
+  const {data} = await api.post('/repositories',{ ...repo })
+  return data
+} 
+
+export const addLikeRepository = async (repoID) => {
+  const {data} = await api.post(`/repositories/${repoID}/like`)
+  return data
+} 
+
 export default api;
